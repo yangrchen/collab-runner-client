a4b3dd4c31a8faa115731a577415c11c6192c016
yangrchen <yangrc.chen@gmail.com>
2025-03-02 19:13:11 -0500

    Editor output as codemirror changes

--- src/lib/components/EditorNode.svelte
+++ src/lib/components/EditorNode.svelte
@@ -4,20 +4,33 @@
 	import { oneDark } from '@codemirror/theme-one-dark';
 	import { EditorState } from '@codemirror/state';
 	import { EditorView, keymap } from '@codemirror/view';
-	import { Handle, NodeResizer, Position, type NodeProps } from '@xyflow/svelte';
+	import {
+		Handle,
+		NodeResizer,
+		Position,
+		type NodeProps,
+		useNodeConnections
+	} from '@xyflow/svelte';
 	import { basicSetup } from 'codemirror';
 
 	type $$Props = NodeProps;
 
-	let { data, isConnectable, selected } = $props();
-	let { header } = data;
+	let { id, data, isConnectable, selected } = $props();
 	let editorView: EditorView;
+	let outputView: EditorView;
 	let output = $state('');
+	let label = $state('');
+
+	const connections = useNodeConnections({ handleType: 'target' });
+	let sourceIds = $derived($connections.map((connection) => connection.source));
 
 	function initializeEditor(element: HTMLDivElement) {
 		if (!element) return;
 
-		const initState = EditorState.create({
+		const editorElement = element.children[0];
+		const outputElement = element.children[1];
+
+		const initEditorState = EditorState.create({
 			doc: '# Start coding here\n',
 			extensions: [
 				basicSetup,
@@ -28,55 +41,87 @@
 			]
 		});
 
+		const initOutputState = EditorState.create({
+			doc: '',
+			extensions: [basicSetup, python(), EditorState.readOnly.of(true), oneDark]
+		});
+
 		editorView = new EditorView({
+			state: initEditorState,
+			parent: editorElement
+		});
+
+		outputView = new EditorView({
+			state: initOutputState,
+			parent: outputElement
+		});
+	}
+
+	function initializeOutputView(element: HTMLDivElement) {
+		if (!element) return;
+
+		const initState = EditorState.create({
+			doc: '',
+			extensions: [basicSetup, oneDark, python(), EditorState.readOnly.of(true)]
+		});
+
+		outputView = new EditorView({
 			state: initState,
 			parent: element
 		});
 	}
 
 	async function handleSubmit() {
-		try {
-			const code = editorView.state.doc.toString();
-			const id = Date.now().toString();
-			const response = await fetch('http://localhost:8080/run-job', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({ id, code })
-			});
-			const data = await response.json();
-			output = data.stdout;
-		} catch (error) {
-			console.error('Error:', error);
-		}
+		// try {
+		// 	const code = editorView.state.doc.toString();
+		// 	const response = await fetch('http://localhost:8080/run-job', {
+		// 		method: 'POST',
+		// 		headers: {
+		// 			'Content-Type': 'application/json'
+		// 		},
+		// 		body: JSON.stringify({ id, code, sourceIds })
+		// 	});
+		// 	const data = await response.json();
+		// 	console.log({ data });
+		// 	output = data.stdout;
+		// } catch (error) {
+		// 	console.error('Error:', error);
+		// }
+		outputView.dispatch({ changes: { from: 0, to: outputView.state.doc.length, insert: editorView.state.doc.toString() } });
 	}
 </script>
 
-<NodeResizer
-	isVisible={selected}
-	onResize={() => {
-		editorView.requestMeasure();
-	}}
-	minWidth={384}
-	maxWidth={480}
-	minHeight={128}
-	maxHeight={320}
-/>
+<NodeResizer isVisible={selected} minWidth={400} maxWidth={720} minHeight={128} maxHeight={320} />
 <Handle type="target" position={Position.Left} style="z-index: 10" {isConnectable} />
-<div class="select-none rounded-t-2xl bg-neutral-900 px-2 text-sm text-white transition-colors">
-	Editor {header}
-</div>
-<div class="flex h-[calc(100%-1.25rem)]">
-	<div
-		class="nodrag w-2/3 cursor-text overflow-hidden rounded-bl-2xl [&_.cm-editor]:h-full [&_.cm-editor]:min-h-32 [&_.cm-editor]:min-w-96 [&_.cm-scroller]:h-full [&_.cm-scroller]:min-h-32 [&_.cm-scroller]:min-w-96"
-		use:initializeEditor
-	></div>
-	<div class="w-1/3 rounded-br-2xl bg-red-50">{output}</div>
-	<button type="submit" class="absolute right-1 top-0" onclick={handleSubmit}>
-		<span class="icon-[line-md--play-twotone] bg-green-500 text-lg hover:bg-green-700"></span>
+<div
+	class="flex w-full select-none items-center justify-between rounded-t-2xl bg-neutral-900 px-2 py-1 text-sm transition-colors"
+>
+	<div class="nodrag flex items-center">
+		<span class="icon-[proicons--python] bg-white text-lg"></span>
+		<input
+			type="text"
+			bind:value={label}
+			placeholder="Untitled"
+			class="ml-2 rounded bg-neutral-900 px-2 text-white hover:bg-gray-700"
+		/>
+	</div>
+	<button type="submit" class="flex" onclick={handleSubmit}>
+		<span class="icon-[line-md--play-twotone] bg-green-500 text-xl hover:bg-green-700"></span>
 	</button>
 </div>
+<div class="flex h-[calc(100%-2rem)]" use:initializeEditor>
+	<div
+		class="nodrag w-1/2 cursor-text overflow-hidden rounded-bl-2xl [&_.cm-editor]:h-full [&_.cm-editor]:min-h-32 [&_.cm-editor]:min-w-[400px] [&_.cm-scroller]:h-full [&_.cm-scroller]:min-h-32 [&_.cm-scroller]:w-full [&_.cm-scroller]:min-w-[400px]"
+	></div>
+	<div
+		class="w-1/2 rounded-br-2xl [&_.cm-editor]:h-full [&_.cm-scroller]:h-full [&_.cm-scroller]:w-full"
+	></div>
+</div>
+{#if sourceIds.length > 0}
+	{#each sourceIds as sourceId}
+		<div class="bg-red-100">{sourceId}</div>
+	{/each}
+{/if}
 <Handle
 	type="source"
 	position={Position.Right}
--- src/lib/components/SvelteTest.svelte
+++ src/lib/components/SvelteTest.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { writable } from 'svelte/store';
+	import { v4 as uuidv4 } from 'uuid';
 	import { SvelteFlow, Background, Controls, type Node, Panel } from '@xyflow/svelte';
 	import '@xyflow/svelte/dist/style.css';
 	import EditorNode from './EditorNode.svelte';
@@ -10,35 +11,26 @@
 
 	const nodes = writable<Node[]>([]);
 	const edges = writable([]);
-	let nodeId = $state(0);
 
 	function addEditorNode() {
+		const nodeId = uuidv4();
 		const newEditor: Node = {
-			id: (nodeId++).toString(),
+			id: nodeId,
 			type: 'editorNode',
-			position: { x: 0, y: 100 },
-			data: { header: nodeId }
+			position: { x: (window.innerWidth - 400) / 2, y: (window.innerHeight - 128) / 2 },
+			data: {}
 		};
 		$nodes = [...$nodes, newEditor];
 	}
 </script>
 
 <div class="h-screen">
-	<SvelteFlow
-		{nodes}
-		{edges}
-		{nodeTypes}
-		ondelete={({ nodes, edges }) => {
-			if (nodes.length != 0) {
-				nodeId--;
-			}
-		}}
-	>
+	<SvelteFlow {nodes} {edges} {nodeTypes} minZoom={0.1} proOptions={{ hideAttribution: true }}>
 		<Panel position="top-right" class="h-full content-center">
-			<div class="h-1/3 rounded bg-gray-800 p-2">
+			<div class="h-1/3 rounded bg-gray-800 p-4">
 				<button
 					onclick={addEditorNode}
-					class="icon-[proicons--python] bg-white text-2xl hover:bg-gray-400"
+					class="icon-[proicons--python] bg-white text-5xl hover:bg-gray-400"
 				></button>
 			</div>
 		</Panel>
